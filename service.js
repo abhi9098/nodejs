c2c46512bc36850ffb58cb096ca5d99a47301e0e
Abhishek Chuahan <54019710+abhi9098@users.noreply.github.com>
2022-10-12 15:55:02 +0530

    Delete service.js

--- service.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3070
-
-app.use('/v1', require('./routes/index'));
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
